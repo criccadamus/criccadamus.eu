7457a604c30aa7c6fc54907a2a96bba84993cfaa
Criccadamus <contact@criccadamus.eu>
2023-05-07 23:00:25 +0200

    Delete resources directory

--- resources/scripts/scroll-to-top-when-reload.js
+++
@@ -1,7 +0,0 @@
-window.onbeforeunload = function () {
-  window.scroll({
-    top: 0, 
-    left: 0, 
-    behavior: 'smooth'
-  });
-}
--- resources/scripts/touch-interaction.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('.button');
-
-button.addEventListener('touchstart', function() {
-  button.classList.add('is-touched');
-});
